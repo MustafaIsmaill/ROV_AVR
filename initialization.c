4a8b105b1b64af6a18897a318adb51a8e497b8e6
Mustafa Ismail <mustafa.el-sayed@hotmail.com>
2017-12-02 15:17:39 +0200

    Added software PWM for 6 thrusters using 16 bit timer

--- initialization.c
+++ initialization.c
@@ -9,17 +9,15 @@
 
 void i2c_init(void){
 	 TWCR |= (1<<TWEN)|(1<<TWIE)|(1<<TWEA)|(1<<TWINT); //enable i2c, enable interrupt, enable ACK, set flag
-	 TWAR = 0x20; //slave address
+	 TWAR = 20; //slave address
 	 TWSR = 0x00; //initialize status register as zero
 }
 
 void timer1_init(void){
 	TCCR1A |= (1<<WGM11)|(1<<COM1A1)|(1<<COM1B1); //fast pwm, ICR as top, non-inverting mode
 	TCCR1B |= (1<<WGM12) | (1<<WGM13) | (1<<CS11); //pre-scale 8
-//	TIMSK1 |= (1<<TOIE1) | (1<<OCIE1A) | (1<<OCIE1B); //interrupts enable
-	ICR1 = 40000; //top value
-	OCR1A = 20000; //pwm 1
-	OCR1B = 10000; //pwm 2
+	TIMSK1 |= (1<<TOIE1) | (1<<OCIE1A) | (1<<OCIE1B); //interrupts enable
+	ICR1 = 39999; //top value
 }
 
 void ports_init(void){
--- main.c
+++ main.c
@@ -9,16 +9,38 @@
 #include <avr/interrupt.h>
 #include "initialization.h"
 
+volatile uint8_t PWMcount = 0;
+volatile const uint16_t compare1[6]={0,6666,13332,19998,26664,33330}; //base value for each thruster
+volatile uint16_t compare2[6]={2880,2880,2880,2880,2880,2880}; //pwm received from i2c
+
+
+volatile uint8_t TWIcount=0;
+volatile uint8_t bytes[2]={0,0};
+volatile uint16_t twoBytes;
 
 int main(void)
 {
 	i2c_init(); //i2c
+	OCR1A = compare1[PWMcount];
 	timer1_init(); //timer1
 	ports_init(); //ports
+ 
 
 	sei(); //global interrupt enable
 
     while(1);
+} 
+
+ISR(TIMER1_COMPA_vect){
+	PORTD |= (1<<PWMcount); //set pin
+	OCR1B = compare1[PWMcount] + compare2[PWMcount]; //set pulse width
+} 
+
+ISR(TIMER1_COMPB_vect){
+	PORTD &= ~(1<<PWMcount); //reset pin
+	if(PWMcount == 5){PWMcount = 0;} //reset counter
+	else{PWMcount++;} //increment counter
+	OCR1A = compare1[PWMcount]; //set base value for the next thruster
 }
 
 ISR(TWI_vect){ //i2c interrupt
@@ -30,10 +52,18 @@ ISR(TWI_vect){ //i2c interrupt
 		case 0x80: //data
 			TWCR |= (1<<TWINT)|(1<<TWEA);
 			TWCR &= ~(1<<TWSTO);
-			PORTB = TWDR; //output data to port D
+			bytes[TWIcount] = TWDR;
+			TWIcount++;
 			break;
 		case 0xA0: //stop or restart
 			TWCR |= (1<<TWINT);
 			break;
+	}
+	if(TWIcount==1){
+		uint8_t lByte = bytes[1];
+		uint8_t hByte = bytes[0];
+		twoBytes = (hByte<<8) | lByte;
+		bytes[0]=0; bytes[1]=0;
+		TWIcount=0;
 	}
 }
